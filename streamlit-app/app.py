23d4882282a813b1daee832a47c2e96ba91eaf82
=Harish <=harishchoudhary1616@gmail.com>
2026-03-10 00:16:16 +0530

    Initial commit

---
+++ streamlit-app/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import helper
+import pickle
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "model.pkl")
+
+model = pickle.load(open(model_path, 'rb'))
+
+st.header('Duplicate Question Pairs')
+
+q1 = st.text_input('Enter question 1')
+q2 = st.text_input('Enter question 2')
+
+if st.button('Find'):
+    query = helper.query_point_creator(q1,q2)
+    result = model.predict(query)[0]
+
+    if result:
+        st.header('Duplicate')
+    else:
+        st.header('Not Duplicate')
+
+
